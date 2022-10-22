74e65b7c028a2f275548997e2c74ca1064c73422
Roman <95884644+RomanTsiukh@users.noreply.github.com>
2022-10-22 08:32:28 +0300

    add files

---
+++ contacts.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const fs = require("fs/promises");
+
+// * Раскомментируй и запиши значение
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+// TODO: задокументировать каждую функцию
+async function listContacts() {
+  // ...твой код
+}
+
+async function getContactById(contactId) {
+  // ...твой код
+}
+
+async function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
