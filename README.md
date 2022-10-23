23b5731321131179c1828b6cf8688b8d102d7d66
Roman <95884644+RomanTsiukh@users.noreply.github.com>
2022-10-23 11:53:22 +0300

    add

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# goit-nodejs-hw-01
+CLI-app
+
+Сontacts list: node index.js --action="list" https://monosnap.com/file/PanNlL4i7nbHy2UdlAZmW65phA2jr7
+
+Get a contact by Id: node index.js --action="get" --id=5 https://monosnap.com/file/lPIKcP9pQsIaB4DkNMIvw4kYMuf8IH
+
+Add contact(name, email, phone): node index.js --action="add" --name="Mango" --email="mango@gmail.com" --phone="322-22-22" https://monosnap.com/file/P9xtbnR8v5cT1sBTE6NeEuWZo1X66n
+
+Remove contact(id): node index.js --action="remove" --id=3 https://monosnap.com/file/ioTPMeKhGbVbvf51g5DqpP3Wwwd3ps
--- contacts.js
+++ contacts.js
@@ -1,22 +1,96 @@
 const path = require("path");
 const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
 
-// * Раскомментируй и запиши значение
 const contactsPath = path.join(__dirname, "./db/contacts.json");
 
-// TODO: задокументировать каждую функцию
 async function listContacts() {
-  // ...твой код
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 async function getContactById(contactId) {
-  // ...твой код
+  try {
+    const contacts = await listContacts();
+    const selectedContact = contacts.find(
+      (contact) => contact.id === contactId
+    );
+
+    if (!selectedContact) {
+      console.log(`There is no contact with ID: ${contactId}`);
+      return;
+    }
+    console.log("Selected contact:", selectedContact);
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 async function removeContact(contactId) {
-  // ...твой код
+  try {
+    const contacts = await listContacts();
+    const selectedContact = contacts.find(
+      (contact) => contact.id === contactId
+    );
+
+    if (!selectedContact) {
+      console.log(`There is no contact with ID: ${contactId}`);
+      return;
+    }
+
+    const filteredContacts = contacts.filter(
+      (contact) => contact.id !== contactId
+    );
+    await fs.writeFile(contactsPath, JSON.stringify(filteredContacts));
+    console.log(`Contact with ID: ${contactId} has been removed successfully!`);
+    return;
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+    const matchedContactName = contacts.find(
+      (contact) => contact.name.toLowerCase() === name.toLowerCase()
+    );
+    const matchedContactEmail = contacts.find(
+      (contact) => contact.email.toLowerCase() === email.toLowerCase()
+    );
+    const matchedContactPhone = contacts.find(
+      (contact) => contact.phone === phone
+    );
+
+    if (matchedContactName) {
+      console.log(`Contact ${name} is already in list!`);
+      return;
+    } else if (matchedContactEmail) {
+      console.log(`Contact with email: ${email} is already in list!`);
+      return;
+    } else if (matchedContactPhone) {
+      console.log(`Contact with phone: ${phone} is already in list!`);
+      return;
+    }
+
+    const newContact = { id: nanoid(), name, email, phone };
+    const newContactList = JSON.stringify([newContact, ...contacts]);
+    await fs.writeFile(contactsPath, newContactList);
+    console.log(`Contact ${name} has been added successfully!`);
+    return;
+  } catch (error) {
+    console.error(error.message);
+  }
 }
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
